c8c4923bebbd39658f46dfd9e2dc23fb5090693c
sdyz5210 <sdyz5210@gmail.com>
2015-06-08 18:55:39 +0800

    增加样式

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -1,16 +1,27 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.Post;
+import play.Play;
+import play.mvc.Before;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		Post frontPost = Post.find("order by postedAt desc").first();
+		List<Post> olderPosts = Post.find("order by postedAt desc").from(1)
+				.fetch(10);
+		render(frontPost, olderPosts);
+	}
+
+	@Before
+	static void addDefaults() {
+		renderArgs.put("blogTitle",
+				Play.configuration.getProperty("blog.title"));
+		renderArgs.put("blogBaseline",
+				Play.configuration.getProperty("blog.baseline"));
+	}
 
 }
